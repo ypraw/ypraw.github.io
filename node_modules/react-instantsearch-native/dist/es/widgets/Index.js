4ef4a5af354628eaae7d6a487bdda082411f7edd
ypraw <dyo.ndutz@gmail.com>
2020-12-06 14:31:31 +0700

    ref(code base) : deleted unused folders and files

--- node_modules/react-instantsearch-native/dist/es/widgets/Index.js
+++
@@ -1,6 +0,0 @@
-import { View } from 'react-native';
-import { createIndex } from 'react-instantsearch-core';
-var Index = createIndex({
-  Root: View
-});
-export default Index;
--- node_modules/react-instantsearch-native/dist/es/widgets/InstantSearch.js
+++
@@ -1,7 +0,0 @@
-import algoliasearch from 'algoliasearch/reactnative';
-import { View } from 'react-native';
-import { createInstantSearch } from 'react-instantsearch-core';
-var InstantSearch = createInstantSearch(algoliasearch, {
-  Root: View
-});
-export default InstantSearch;
