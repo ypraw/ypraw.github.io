200a2c180788ac4ba5b704f55d6db7f7c95f29df
ypraw <dyo.ndutz@gmail.com>
2021-07-21 14:30:19 +0700

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,25 @@ module.exports = {
         component: require.resolve(`./src/layouts/`)
       }
     },
+     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "process.env.GOOGLE_ANALYTICS_ID", // Google Analytics / GA
+        ],
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
     {
       resolve: `gatsby-plugin-algolia`,
       options: {
